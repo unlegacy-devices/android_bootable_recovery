3cef72db3186a0f2229c56d3e8992b18db82854b
dkati <dkatikaridis@gmail.com>
2019-04-27 00:46:43 +0300

    libtar: Set -O3

--- libtar/Android.mk
+++ libtar/Android.mk
@@ -12,7 +12,7 @@ LOCAL_SHARED_LIBRARIES += libz libc
 
 LOCAL_C_INCLUDES += external/libselinux/include
 LOCAL_SHARED_LIBRARIES += libselinux
-
+LOCAL_CFLAGS += -O3
 ifeq ($(TW_INCLUDE_CRYPTO_FBE), true)
     LOCAL_SHARED_LIBRARIES += libe4crypt
     LOCAL_CFLAGS += -DHAVE_EXT4_CRYPT
@@ -26,6 +26,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libtar_static
 LOCAL_MODULE_TAGS := eng optional
+LOCAL_CFLAGS += -O3
 LOCAL_SRC_FILES := append.c block.c decode.c encode.c extract.c handle.c output.c util.c wrapper.c basename.c strmode.c libtar_hash.c libtar_list.c dirname.c android_utils.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
                     external/zlib
